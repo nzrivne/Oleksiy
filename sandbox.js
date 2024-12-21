ab47be3f33d5b1c6dba2fc30a2f990e98e5200d9
nzrivne <130063546+nzrivne@users.noreply.github.com>
2024-12-21 21:52:55 +0200

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+const hidden = document.getElementById('hidden-menu');
+const hiddenButton = document.getElementById('hidden-img');
+const stay = document.getElementById('hidden-stay');
+hiddenButton.addEventListener('mouseover', () => hidden.style.display = "flex");
+stay.addEventListener('mouseover', () => hidden.style.display = "flex");
+hidden.addEventListener('mouseleave', () => hidden.style.display = "none");
