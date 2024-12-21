0e9d69a2ad820c980d7ea02449916ad2a15c56ff
nzrivne <130063546+nzrivne@users.noreply.github.com>
2024-12-21 23:10:19 +0200

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -14,4 +14,8 @@ const stay = document.getElementById('hidden-stay');
 hiddenButton.addEventListener('mouseover', () => hidden.style.display = "flex");
 stay.addEventListener('mouseover', () => hidden.style.display = "flex");
 hidden.addEventListener('mouseleave', () => hidden.style.display = "none");
-go.addEventListener('scroll', () => hidden.style.display = "none");
+
+function mOver(obj) {
+    const hidden = document.getElementById('hidden-menu');
+    hidden.style.display = "none";
+  }
